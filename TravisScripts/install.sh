17df17216d5ae54345242b5dee73ae93cb7b9831
Rajat Kalsy <crimsonred@users.noreply.github.com>
2017-10-23 19:30:41 +0530

    travis scripts updates

--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -39,7 +39,7 @@ mkdir ~/Library/Unity/Certificates
 cp CACerts.pem ~/Library/Unity/Certificates/
 
 echo "activate license"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -username ${UNITYCI_USER_NAME} -password ${UNITYCI_PASS} -logfile
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -serial ${UNITYCI_SERIAL} -username ${UNITYCI_USER_NAME} -password ${UNITYCI_PASS} -logfile
 
 cat ~/Library/Logs/Unity/Editor.log
 
