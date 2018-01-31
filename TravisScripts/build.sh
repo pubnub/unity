1f44b8ee6865fa22716016a9bda0e2be21226966
Rajat Kalsy <crimsonred@users.noreply.github.com>
2018-01-31 17:04:27 +0530

    testPlatform editmode

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -14,6 +14,7 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME} editmode"
  	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
  	-runEditorTests \
  	-editorTestsResultFile $(pwd)/test1.xml \
+ 	-testPlatform editmode \
  	-username ${UNITYCI_USER_NAME} \
  	-password ${UNITYCI_PASS} \
  	-serial ${UNITYCI_SERIAL} 
