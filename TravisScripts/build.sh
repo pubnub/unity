85d2a9f39e76134ec5c0419daf4698dd5e9adff4
Rajat Kalsy <crimsonred@users.noreply.github.com>
2018-01-31 12:20:47 +0530

    -testResults: command not found fix

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -13,9 +13,7 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME} editmode"
  	-logFile $(pwd)/editor.log \
  	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
  	-runTests
- 	-testResults $(pwd)/test1.xml \
  	-runEditorTests \
- 	-editorTestsResultFile \
  	-testPlatform editmode \
  	-username ${UNITYCI_USER_NAME} \
  	-password ${UNITYCI_PASS} \
