72d188a7f291aa456a9fef106f3e8a7f1c490696
Rajat Kalsy <crimsonred@users.noreply.github.com>
2018-02-28 16:08:53 +0530

    Bump Version

--- PubNubUnity/Assets/PubNubUnity/PubNubUnityBase.cs
+++ PubNubUnity/Assets/PubNubUnity/PubNubUnityBase.cs
@@ -7,7 +7,7 @@ namespace PubNubAPI
     public class PubNubUnityBase
     {
         protected Counter publishMessageCounter;
-        private const string build = "4.0-beta6";
+        private const string build = "4.0-rc2";
         private string pnsdkVersion = string.Format ("PubNub-CSharp-Unity/{0}", build);
 
         public string Version {
