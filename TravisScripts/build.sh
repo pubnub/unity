3aecbc96b88ba4d1a3570dae5dabb66d51d2e33f
Rajat Kalsy <crimsonred@users.noreply.github.com>
2018-08-22 17:18:24 +0530

    remove travis_wait

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -6,25 +6,6 @@
 #  -projectPath argument to point to the right location.
 
 ## Run the editor unit tests
-echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME} editmode"
-
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-logFile $(pwd)/editor1.log \
-	-projectPath $(pwd)/${UNITYCI_PROJECT_NAME} \
-	-runTests \
-	-testResults $(pwd)/test1.xml \
-	-testPlatform editmode \
-	-username ${UNITYCI_USER_NAME} \
-	-password ${UNITYCI_PASS} \
-	-serial ${UNITYCI_SERIAL}
-
-rc0=$?
-echo "Unit test logs"
-#cat $(pwd)/editor1.log
-cat $(pwd)/test1.xml
-#exit if tests failed
-if [ $rc0 -ne 0 ]; then { echo "Failed unit tests editmode"; /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense; exit $rc0; } fi	
 
 echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME} playmode"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
