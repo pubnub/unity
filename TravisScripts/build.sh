2909feaa6f2f8802c2e14cf11500262a9aa29148
Rajat Kalsy <crimsonred@users.noreply.github.com>
2018-11-09 12:04:41 +0530

    travis script updates

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -32,6 +32,31 @@ echo "returning license"
 #if [ $rc0 -ne 0 ]; then { echo "Failed unit tests editmode"; /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense; exit $rc0; } fi	
 if [ $rc1 -ne 0 ]; then { echo "Failed unit tests editmode"; exit $rc1; } fi	
 
+echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME} playmode"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-batchmode \
+	-logFile $(pwd)/editor2.log \
+	-projectPath $(pwd)/${UNITYCI_PROJECT_NAME} \
+	-runTests \
+	-testResults $(pwd)/test2.xml \
+	-testPlatform playmode \
+	-username ${UNITYCI_USER_NAME} \
+	-password ${UNITYCI_PASS} \
+	-serial ${UNITYCI_SERIAL}
+
+rc1=$?
+echo "Unit test logs 2"
+#cat $(pwd)/editor2.log
+cat $(pwd)/test2.xml
+
+# returning license
+echo "returning license"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense
+
+#exit if tests failed
+#if [ $rc1 -ne 0 ]; then { echo "Failed unit tests playmode"; /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense; exit $rc1; } fi	
+if [ $rc1 -ne 0 ]; then { echo "Failed unit tests playmode"; exit $rc1; } fi	
+
 echo "creating exportPackage"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-quit \
