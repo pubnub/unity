d0fcd5da38f1037a1cf076d36a2e978219340bad
Rajat Kalsy <crimsonred@users.noreply.github.com>
2019-04-23 18:04:02 +0530

    Updates for Unity 2019

--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -2,13 +2,13 @@
 
 # Downloads
 echo 'Downloading Unity pkg:'
-curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/24bbd83e8b9e/MacEditorInstaller/Unity-2018.1.9f1.pkg
+curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/292b93d75a2c/MacEditorInstaller/Unity.pkg
 if [ $? -ne 0 ]; then { echo "Unity Download failed"; exit $?; } fi
 
-echo 'Downloading StandardAssets pkg:'
+#echo 'Downloading StandardAssets pkg:'
 #curl --retry 5 -o Unity_StandardAssets.pkg https://download.unity3d.com/download_unity/46dda1414e51/MacStandardAssetsInstaller/StandardAssets-2017.2.0f3.pkg
-curl --retry 5 -o Unity_StandardAssets.pkg https://download.unity3d.com/download_unity/24bbd83e8b9e/MacStandardAssetsInstaller/StandardAssets-2018.1.9f1.pkg
-if [ $? -ne 0 ]; then { echo "Unity StandardAssets Download failed"; exit $?; } fi
+#curl --retry 5 -o Unity_StandardAssets.pkg https://download.unity3d.com/download_unity/24bbd83e8b9e/MacStandardAssetsInstaller/StandardAssets-2018.1.9f1.pkg
+#if [ $? -ne 0 ]; then { echo "Unity StandardAssets Download failed"; exit $?; } fi
 
 # Install
 echo 'Installing Unity pkg'
@@ -23,8 +23,8 @@ mkdir ~/Library/Unity/Certificates
 
 cp "./TravisScripts/CACerts.pem" ~/Library/Unity/Certificates/
 
-echo === Done ===
-echo 'Installing StandardAssets pkg'
-sudo installer -dumplog -package Unity_StandardAssets.pkg -target /
+#echo === Done ===
+#echo 'Installing StandardAssets pkg'
+#sudo installer -dumplog -package Unity_StandardAssets.pkg -target /
 
 
