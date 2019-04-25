f7cd73603d6ca6cfb4fd1a20bb25425d45d7dd04
Rajat Kalsy <crimsonred@users.noreply.github.com>
2019-04-25 09:49:29 +0530

    Trying with Unity 2018.3.6

--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -2,7 +2,7 @@
 
 # Downloads
 echo 'Downloading Unity pkg:'
-curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/06548a9e9582/MacEditorInstaller/Unity.pkg
+curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/a220877bc173/MacEditorInstaller/Unity.pkg
 if [ $? -ne 0 ]; then { echo "Unity Download failed"; exit $?; } fi
 
 #echo 'Downloading StandardAssets pkg:'
