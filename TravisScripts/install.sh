99712fa30e26b64051cd4b4bc01e9074e93c650a
Rajat Kalsy <crimsonred@users.noreply.github.com>
2019-05-31 15:34:26 +0530

    Remove editor.log output

--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -3,23 +3,23 @@
 # Downloads
 echo 'Downloading Unity pkg:'
 
-curl -v http://license.unity3d.com/ > $(pwd)/license.txt
-cat $(pwd)/license.txt
+# curl -v http://license.unity3d.com/ > $(pwd)/license.txt
+# cat $(pwd)/license.txt
 
-curl -v http://activation.unity3d.com/ > $(pwd)/activation.txt
-cat $(pwd)/activation.txt
+# curl -v http://activation.unity3d.com/ > $(pwd)/activation.txt
+# cat $(pwd)/activation.txt
 
-curl -v http://sl-http.unity3d.com/ > $(pwd)/sl-http.txt
-cat $(pwd)/sl-http.txt
+# curl -v http://sl-http.unity3d.com/ > $(pwd)/sl-http.txt
+# cat $(pwd)/sl-http.txt
 
-curl -v http://developer.cloud.unity3d.com/ > $(pwd)/developer.txt
-cat $(pwd)/developer.txt
+# curl -v http://developer.cloud.unity3d.com/ > $(pwd)/developer.txt
+# cat $(pwd)/developer.txt
 
-curl -v http://core.cloud.unity3d.com/ > $(pwd)/core.txt
-cat $(pwd)/core.txt
+# curl -v http://core.cloud.unity3d.com/ > $(pwd)/core.txt
+# cat $(pwd)/core.txt
 
-curl -v http://accounts.unity3d.com > $(pwd)/accounts.txt
-cat $(pwd)/accounts.txt
+# curl -v http://accounts.unity3d.com > $(pwd)/accounts.txt
+# cat $(pwd)/accounts.txt
 
 #2019.1.4
 #curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/ffa3a7a2dd7d/MacEditorInstaller/Unity.pkg
