5ee4f77396b3ace9bbc6486d1c65c66e1b639d0b
Rajat Kalsy <crimsonred@users.noreply.github.com>
2019-11-11 13:29:04 +0530

    Unity update 7

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -34,7 +34,6 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME} editmode"
 
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
-  -runTests \
   -projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
   -testResults $(pwd)/test1.xml \
   -username "${UNITYCI_NEW_USER}" \
