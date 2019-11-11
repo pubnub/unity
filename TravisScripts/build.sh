069d974986deb58a861e28969f5bbb0b1491e350
Rajat Kalsy <crimsonred@users.noreply.github.com>
2019-11-11 13:03:31 +0530

    Unity update 3

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -34,6 +34,8 @@ echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME} editmode"
 
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
+  -nographics \ 
+  -silent-crashes \ 
   -runEditorTests \
   -logFile $(pwd)/editor1.log \
   -projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
