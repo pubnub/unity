83c21da6ab7e9c088e3789e84e7e7a40b61c7ec6
Rajat Kalsy <crimsonred@users.noreply.github.com>
2019-11-11 15:10:56 +0530

    Tests optimizations

--- AppVeyorScripts/install.ps1
+++ AppVeyorScripts/install.ps1
@@ -1,4 +1,4 @@
-$source = "https://download.unity3d.com/download_unity/292b93d75a2c/Windows64EditorInstaller/UnitySetup64.exe"
+$source = "https://download.unity3d.com/download_unity/5f859a4cfee5/Windows64EditorInstaller/UnitySetup64.exe"
 $destination = ".\UnitySetup64.exe"
 Invoke-WebRequest $source -OutFile $destination
 Start-Process -FilePath ".\UnitySetup64.exe" -Wait -ArgumentList ('/S', '/Q')
