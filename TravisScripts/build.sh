f0a1d339e03f556700b04b8e043c1e5bdda3a673
Rajat Kalsy <crimsonred@users.noreply.github.com>
2020-03-11 16:57:19 +0530

    Travis package upload fix 6

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -5,7 +5,7 @@
 #  the project folder is "UnityProject". If this is not true then adjust the 
 #  -projectPath argument to point to the right location.
 
-# Run the editor unit tests
+## Run the editor unit tests
 echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME} editmode"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
@@ -28,34 +28,34 @@ cat $(pwd)/test1.xml
 echo "returning license"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense
 
-# #exit if tests failed
-# #if [ $rc0 -ne 0 ]; then { echo "Failed unit tests editmode"; /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense; exit $rc0; } fi	
-# if [ $rc0 -ne 0 ]; then { echo "Failed unit tests editmode"; exit $rc0; } fi	
+#exit if tests failed
+#if [ $rc0 -ne 0 ]; then { echo "Failed unit tests editmode"; /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense; exit $rc0; } fi	
+if [ $rc0 -ne 0 ]; then { echo "Failed unit tests editmode"; exit $rc0; } fi	
 
-# echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME} playmode"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-# 	-batchmode \
-# 	-logFile $(pwd)/editor2.log \
-# 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-# 	-runEditorTests \
-# 	-testResults $(pwd)/test2.xml \
-# 	-testPlatform playmode \
-# 	-username "${UNITYCI_NEW_USER}" \
-# 	-password "${UNITYCI_NEW_PASS}" \
-# 	-serial "${UNITYCI_NEW_SERIAL}" 
+echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME} playmode"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-batchmode \
+	-logFile $(pwd)/editor2.log \
+	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+	-runEditorTests \
+	-testResults $(pwd)/test2.xml \
+	-testPlatform playmode \
+	-username "${UNITYCI_NEW_USER}" \
+	-password "${UNITYCI_NEW_PASS}" \
+	-serial "${UNITYCI_NEW_SERIAL}" 
 
-# rc1=$?
-# echo "Unit test logs 2"
-# #cat $(pwd)/editor2.log
-# cat $(pwd)/test2.xml
+rc1=$?
+echo "Unit test logs 2"
+#cat $(pwd)/editor2.log
+cat $(pwd)/test2.xml
 
-# # returning license
-# echo "returning license"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense
+# returning license
+echo "returning license"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense
 
-# #exit if tests failed
-# #if [ $rc1 -ne 0 ]; then { echo "Failed unit tests playmode"; /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense; exit $rc1; } fi	
-# if [ $rc1 -ne 0 ]; then { echo "Failed unit tests playmode"; exit $rc1; } fi	
+#exit if tests failed
+#if [ $rc1 -ne 0 ]; then { echo "Failed unit tests playmode"; /Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense; exit $rc1; } fi	
+if [ $rc1 -ne 0 ]; then { echo "Failed unit tests playmode"; exit $rc1; } fi	
 
 echo "creating exportPackage"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
