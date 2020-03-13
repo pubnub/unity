5dd4346b3444eae95ae66f563c4c2f4404fb1ba5
Rajat Kalsy <crimsonred@users.noreply.github.com>
2020-03-13 14:48:31 +0530

    Travis update fix 15

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -59,13 +59,14 @@
 
 echo "creating exportPackage"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-quit \
 	-batchmode \
 	-logFile $(pwd)/exportPackage.log \
 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
 	-exportPackage "Assets" "${UNITYCI_PACKAGE_NAME}.unitypackage" \
-	-username "${UNITYCI_USER_NAME}" \
-	-password "${UNITYCI_PASS}" \
-	-serial "${UNITYCI_SERIAL}" \
+	-username "${UNITYCI_NEW_USER}" \
+	-password "${UNITYCI_NEW_PASS}" \
+	-serial "${UNITYCI_NEW_SERIAL}" 
 
 # returning license
 echo "returning license"
