dd14176f8eeeb482ef198898d6806f4283bb153a
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2022-10-05 13:24:03 +0200

    fix: refactor lambda

--- PubNubUnity/Assets/Utils/Editor/PubNubPackageExporter.cs
+++ PubNubUnity/Assets/Utils/Editor/PubNubPackageExporter.cs
@@ -8,9 +8,9 @@ public class PubNubPackageExporter : MonoBehaviour {
 
 	[MenuItem("Assets/Export PubNub Package")]
 	public static void ExportPackage() {
-		System.Func<string, bool> assetTestsFilter = (string s) => !s.StartsWith("Assets/PubNub/PlayModeTests");
-		
-		var assets = AssetDatabase.FindAssets("", new[] { "Assets/PubNub" }).Select(AssetDatabase.GUIDToAssetPath).Where(assetTestsFilter).ToArray();
+		bool AssetTestsFilter(string s) => !s.StartsWith("Assets/PubNub/PlayModeTests");
+
+		var assets = AssetDatabase.FindAssets("", new[] { "Assets/PubNub" }).Select(AssetDatabase.GUIDToAssetPath).Where((System.Func<string, bool>)AssetTestsFilter).ToArray();
 
 		Debug.Log("Folders to be exported:\n" + string.Join(", ", assets));
 
