e5abe21ad9d3f2b9bb6c04b1af43622bd059faa3
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2022-10-05 13:25:33 +0200

    fix: redundant cast

--- PubNubUnity/Assets/Utils/Editor/PubNubPackageExporter.cs
+++ PubNubUnity/Assets/Utils/Editor/PubNubPackageExporter.cs
@@ -8,9 +8,9 @@ public class PubNubPackageExporter : MonoBehaviour {
 
 	[MenuItem("Assets/Export PubNub Package")]
 	public static void ExportPackage() {
-		bool AssetTestsFilter(string s) => !s.StartsWith("Assets/PubNub/PlayModeTests");
+		bool assetTestsFilter(string s) => !s.StartsWith("Assets/PubNub/PlayModeTests");
 
-		var assets = AssetDatabase.FindAssets("", new[] { "Assets/PubNub" }).Select(AssetDatabase.GUIDToAssetPath).Where((System.Func<string, bool>)AssetTestsFilter).ToArray();
+		var assets = AssetDatabase.FindAssets("", new[] { "Assets/PubNub" }).Select(AssetDatabase.GUIDToAssetPath).Where(assetTestsFilter).ToArray();
 
 		Debug.Log("Folders to be exported:\n" + string.Join(", ", assets));
 
