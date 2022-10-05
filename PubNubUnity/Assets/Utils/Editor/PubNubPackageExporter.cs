86b18310b749c7fba0f00b441d4c47d20e27a5da
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2022-10-05 13:43:24 +0200

    fix: naming

--- PubNubUnity/Assets/Utils/Editor/PubNubPackageExporter.cs
+++ PubNubUnity/Assets/Utils/Editor/PubNubPackageExporter.cs
@@ -6,9 +6,9 @@ public class PubNubPackageExporter : MonoBehaviour {
 
 	[MenuItem("Assets/Export PubNub Package")]
 	public static void ExportPackage() {
-		bool assetTestsFilter(string s) => !s.StartsWith("Assets/PubNub/PlayModeTests");
+		bool assetFilter(string s) => !s.StartsWith("Assets/PubNub/PlayModeTests");
 
-		var assets = AssetDatabase.FindAssets("", new[] { "Assets/PubNub" }).Select(AssetDatabase.GUIDToAssetPath).Where(assetTestsFilter).ToArray();
+		var assets = AssetDatabase.FindAssets("", new[] { "Assets/PubNub" }).Select(AssetDatabase.GUIDToAssetPath).Where(assetFilter).ToArray();
 
 		Debug.Log("Assets to be exported:\n" + string.Join(", ", assets));
 
