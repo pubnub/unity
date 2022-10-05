080fb0a6879e334a9c146c8bddfd6f0d6899aeb1
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2022-10-05 13:17:13 +0200

    fix: class name

--- PubNubUnity/Assets/Utils/Editor/PubNubPackageExporter.cs
+++ PubNubUnity/Assets/Utils/Editor/PubNubPackageExporter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEditor;
 using System.Linq;
 
-public class ExportPubNubPackage : MonoBehaviour {
+public class PubNubPackageExporter : MonoBehaviour {
 
 	[MenuItem("Assets/Export PubNub Package")]
 	public static void ExportPackage() {
@@ -12,7 +12,7 @@ public class ExportPubNubPackage : MonoBehaviour {
 		
 		var assets = AssetDatabase.FindAssets("", new[] { "Assets/PubNub" }).Select(AssetDatabase.GUIDToAssetPath).Where(assetTestsFilter).ToArray();
 
-		Debug.Log("Folders to be exported:" + string.Join(", ", assets));
+		Debug.Log("Folders to be exported:\n" + string.Join(", ", assets));
 
 		AssetDatabase.ExportPackage(assets, "PubNub.unitypackage", ExportPackageOptions.Recurse);
 	}
