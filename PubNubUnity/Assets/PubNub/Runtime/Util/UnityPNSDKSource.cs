20034df50e68855fcfafd197e33d3e4e161fb505
PUBNUB\jakub.grzesiowski <jakub.grzesiowski@pubnub.com>
2024-12-19 15:34:36 +0100

    WIP versioning

--- PubNubUnity/Assets/PubNub/Runtime/Util/UnityPNSDKSource.cs
+++ PubNubUnity/Assets/PubNub/Runtime/Util/UnityPNSDKSource.cs
@@ -6,7 +6,23 @@ namespace PubnubApi.Unity
     public class UnityPNSDKSource : IPNSDKSource
     {
 	    public string GetPNSDK() {
-		    throw new NotImplementedException();
+			#if(UNITY_IOS)
+			        Version = string.Format("PubNub-CSharp-UnityIOS/{0}", build);
+			#elif(UNITY_STANDALONE_WIN)
+			        Version = string.Format("PubNub-CSharp-UnityWin/{0}", build);
+			#elif(UNITY_STANDALONE_OSX)
+			        Version = string.Format("PubNub-CSharp-UnityOSX/{0}", build);
+			#elif(UNITY_ANDROID)
+			        Version = string.Format("PubNub-CSharp-UnityAndroid/{0}", build);
+			#elif(UNITY_STANDALONE_LINUX)
+			        Version = string.Format("PubNub-CSharp-UnityLinux/{0}", build);
+			#elif(UNITY_WEBPLAYER)
+			        Version = string.Format("PubNub-CSharp-UnityWeb/{0}", build);
+			#elif(UNITY_WEBGL)
+					Version = string.Format("PubNub-CSharp-UnityWebGL/{0}", build);
+			#else
+			        Version = string.Format("PubNub-CSharp-Unity/{0}", build);
+			#endif
 	    }
     }
 }
