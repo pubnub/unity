d86b2ab266d0eb270a93477076ec5f89a66f9f9b
PUBNUB\jakub.grzesiowski <jakub.grzesiowski@pubnub.com>
2024-12-19 15:24:20 +0100

    Import DLL with IPNSDKSource, preapre Unity side implementation

---
+++ PubNubUnity/Assets/PubNub/Runtime/Util/UnityPNSDKSource.cs
@@ -0,0 +1,12 @@
+using System;
+using PubnubApi.PNSDK;
+
+namespace PubnubApi.Unity
+{
+    public class UnityPNSDKSource : IPNSDKSource
+    {
+	    public string GetPNSDK() {
+		    throw new NotImplementedException();
+	    }
+    }
+}
