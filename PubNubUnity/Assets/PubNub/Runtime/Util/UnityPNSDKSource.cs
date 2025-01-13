6e2c2a5eb5cbe1bd6c12c606770ff5821f892805
PUBNUB\jakub.grzesiowski <jakub.grzesiowski@pubnub.com>
2025-01-13 13:10:07 +0100

    Fix PNSDK source script

--- PubNubUnity/Assets/PubNub/Runtime/Util/UnityPNSDKSource.cs
+++ PubNubUnity/Assets/PubNub/Runtime/Util/UnityPNSDKSource.cs
@@ -3,25 +3,27 @@ using PubnubApi.PNSDK;
 
 namespace PubnubApi.Unity
 {
-    public class UnityPNSDKSource : IPNSDKSource
-    {
+    public class UnityPNSDKSource : IPNSDKSource {
+
+	    private const string build = "v8.0.0";
+
 	    public string GetPNSDK() {
 			#if(UNITY_IOS)
-			        Version = string.Format("PubNub-CSharp-UnityIOS/{0}", build);
+			        return string.Format("PubNub-CSharp-UnityIOS/{0}", build);
 			#elif(UNITY_STANDALONE_WIN)
-			        Version = string.Format("PubNub-CSharp-UnityWin/{0}", build);
+					return string.Format("PubNub-CSharp-UnityWin/{0}", build);
 			#elif(UNITY_STANDALONE_OSX)
-			        Version = string.Format("PubNub-CSharp-UnityOSX/{0}", build);
+			        return string.Format("PubNub-CSharp-UnityOSX/{0}", build);
 			#elif(UNITY_ANDROID)
-			        Version = string.Format("PubNub-CSharp-UnityAndroid/{0}", build);
+			        return string.Format("PubNub-CSharp-UnityAndroid/{0}", build);
 			#elif(UNITY_STANDALONE_LINUX)
-			        Version = string.Format("PubNub-CSharp-UnityLinux/{0}", build);
+			        return string.Format("PubNub-CSharp-UnityLinux/{0}", build);
 			#elif(UNITY_WEBPLAYER)
-			        Version = string.Format("PubNub-CSharp-UnityWeb/{0}", build);
+			        return string.Format("PubNub-CSharp-UnityWeb/{0}", build);
 			#elif(UNITY_WEBGL)
-					Version = string.Format("PubNub-CSharp-UnityWebGL/{0}", build);
+					return string.Format("PubNub-CSharp-UnityWebGL/{0}", build);
 			#else
-			        Version = string.Format("PubNub-CSharp-Unity/{0}", build);
+			        return string.Format("PubNub-CSharp-Unity/{0}", build);
 			#endif
 	    }
     }
