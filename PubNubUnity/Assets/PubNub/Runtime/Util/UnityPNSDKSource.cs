6da33ecf745e7440bfc0d78bbc9af38cb6eeb10f
jakub-grzesiowski <162123404+jakub-grzesiowski@users.noreply.github.com>
2025-05-08 20:51:10 +0200

    PubNub Unity v9.0.0 release (#117)
    * PubNub SDK v9.0.0 release.

--- PubNubUnity/Assets/PubNub/Runtime/Util/UnityPNSDKSource.cs
+++ PubNubUnity/Assets/PubNub/Runtime/Util/UnityPNSDKSource.cs
@@ -5,7 +5,7 @@ namespace PubnubApi.Unity
 {
     public class UnityPNSDKSource : IPNSDKSource {
 
-	    private const string build = "8.2.0";
+	    private const string build = "9.0.0";
 
 	    public string GetPNSDK() {
 			#if(UNITY_IOS)
